13bb86e3b77eb64e25a12901793e1e99faa99795
Vitaliy Sulimenko <prevetal@gmail.com>
2025-08-03 11:47:48 +0400

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,515 @@
+WW = window.innerWidth || document.clientWidth || document.getElementsByTagName('body')[0].clientWidth
+WH = window.innerHeight || document.clientHeight || document.getElementsByTagName('body')[0].clientHeight
+BODY = document.getElementsByTagName('body')[0]
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	// Main slider
+	let mainSlider = document.querySelector('.main_slider .swiper')
+
+	if (mainSlider) {
+		new Swiper('.main_slider .swiper', {
+			loop: true,
+			speed: 750,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			spaceBetween: 0,
+			slidesPerView: 1,
+			loopAdditionalSlides: 1,
+			pagination: {
+				el: '.swiper-pagination',
+				type: 'bullets',
+				clickable: true,
+				bulletActiveClass: 'active'
+			},
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			lazy: true
+		})
+	}
+
+
+	// Logos slider
+	const logosSliders = [],
+		logos = document.querySelectorAll('.logos .swiper')
+
+	logos.forEach((el, i) => {
+		el.classList.add('logos_s' + i)
+
+		let options = {
+			loop: false,
+			loopAdditionalSlides: 1,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			lazy: true,
+			breakpoints: {
+				0: {
+					spaceBetween: 24,
+					slidesPerView: 'auto'
+				},
+				768: {
+					spaceBetween: 24,
+					slidesPerView: 3
+				},
+				1024: {
+					spaceBetween: 24,
+					slidesPerView: 4
+				},
+				1280: {
+					spaceBetween: 30,
+					slidesPerView: 5
+				}
+			}
+		}
+
+		logosSliders.push(new Swiper('.logos_s' + i, options))
+	})
+
+
+	// Reviews slider
+	const reviewsSliders = [],
+		reviews = document.querySelectorAll('.reviews .swiper')
+
+	reviews.forEach((el, i) => {
+		el.classList.add('reviews_s' + i)
+
+		let options = {
+			loop: false,
+			loopAdditionalSlides: 1,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			slidesPerView: 1,
+			breakpoints: {
+				0: {
+					spaceBetween: 12
+				},
+				480: {
+					spaceBetween: 12
+				},
+				768: {
+					spaceBetween: 12
+				},
+				1024: {
+					spaceBetween: 12
+				},
+				1280: {
+					spaceBetween: 30
+				}
+			},
+		}
+
+		reviewsSliders.push(new Swiper('.reviews_s' + i, options))
+	})
+
+
+	// Products slider
+	const productsSliders = [],
+		products = document.querySelectorAll('.products_slider .swiper')
+
+	products.forEach((el, i) => {
+		el.classList.add('products_s' + i)
+
+		let options = {
+			loop: false,
+			loopAdditionalSlides: 1,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			lazy: true,
+			breakpoints: {
+				0: {
+					spaceBetween: 12,
+					slidesPerView: 'auto'
+				},
+				480: {
+					spaceBetween: 12,
+					slidesPerView: 2
+				},
+				768: {
+					spaceBetween: 12,
+					slidesPerView: 3
+				},
+				1024: {
+					spaceBetween: 12,
+					slidesPerView: 4
+				},
+				1280: {
+					spaceBetween: 30,
+					slidesPerView: 4
+				}
+			},
+			on: {
+				init: swiper => setHeight(swiper.el.querySelectorAll('.product')),
+				resize: swiper => {
+					let items = swiper.el.querySelectorAll('.product')
+
+					items.forEach(el => el.style.height = 'auto')
+
+					setHeight(products)
+				}
+			}
+		}
+
+		productsSliders.push(new Swiper('.products_s' + i, options))
+	})
+
+
+	// Articles slider
+	const articlesSliders = [],
+		articles = document.querySelectorAll('.articles_block .swiper')
+
+	articles.forEach((el, i) => {
+		el.classList.add('articles_s' + i)
+
+		let options = {
+			loop: false,
+			loopAdditionalSlides: 1,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			lazy: true,
+			slidesPerView: 1,
+			breakpoints: {
+				0: {
+					spaceBetween: 64
+				},
+				1280: {
+					spaceBetween: 89
+				}
+			},
+			on: {
+				init: swiper => setHeight(swiper.el.querySelectorAll('.article')),
+				resize: swiper => {
+					let items = swiper.el.querySelectorAll('.article')
+
+					items.forEach(el => el.style.height = 'auto')
+
+					setHeight(products)
+				}
+			}
+		}
+
+		articlesSliders.push(new Swiper('.articles_s' + i, options))
+	})
+
+
+	// Product info
+	if ($('.product_info .images').length) {
+		const productThumbs = new Swiper('.product_info .thumbs .swiper', {
+			loop: true,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			lazy: true,
+			spaceBetween: 10,
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			breakpoints: {
+				0: {
+					slidesPerView: 4
+				},
+				768: {
+					slidesPerView: 4
+				},
+				1024: {
+					slidesPerView: 4
+				},
+				1280: {
+					slidesPerView: 4
+				}
+			}
+		})
+
+		new Swiper('.product_info .big .swiper', {
+			loop: true,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			spaceBetween: 24,
+			slidesPerView: 1,
+			lazy: true,
+			thumbs: {
+				swiper: productThumbs
+			}
+		})
+	}
+
+
+	// Fancybox
+	Fancybox.defaults.autoFocus = false
+	Fancybox.defaults.trapFocus = false
+	Fancybox.defaults.dragToClose = false
+	Fancybox.defaults.placeFocusBack = false
+	Fancybox.defaults.l10n = {
+		CLOSE: 'Закрыть',
+		NEXT: 'Следующий',
+		PREV: 'Предыдущий',
+		MODAL: 'Вы можете закрыть это модальное окно нажав клавишу ESC'
+	}
+
+	Fancybox.defaults.tpl = {
+		closeButton: '<button data-fancybox-close class="f-button is-close-btn" title="{{CLOSE}}"><svg><use xlink:href="images/sprite.svg#ic_close"></use></svg></button>',
+
+		main: `<div class="fancybox__container" role="dialog" aria-modal="true" aria-label="{{MODAL}}" tabindex="-1">
+			<div class="fancybox__backdrop"></div>
+			<div class="fancybox__carousel"></div>
+			<div class="fancybox__footer"></div>
+		</div>`,
+	}
+
+
+	// Modals
+	$('.modal_btn').click(function(e) {
+		e.preventDefault()
+
+		Fancybox.close()
+
+		Fancybox.show([{
+			src: document.getElementById(e.target.getAttribute('data-modal')),
+			type: 'inline'
+		}])
+	})
+
+
+	// Zoom images
+	Fancybox.bind('.fancy_img', {
+		Image: {
+			zoom: false
+		},
+		Thumbs: {
+			autoStart: false
+		}
+	})
+
+
+	// Mob. menu
+	$('.mob_header .mob_menu_btn, header .mob_close_btn, .overlay').click((e) => {
+		e.preventDefault()
+
+		$('.mob_header .mob_menu_btn').toggleClass('active')
+		$('body').toggleClass('lock')
+		$('header').toggleClass('show')
+
+		$('.mob_header .mob_menu_btn').hasClass('active')
+			? $('.overlay').fadeIn(300)
+			: $('.overlay').fadeOut(300)
+	})
+
+
+	// 'Up' button
+	$('.buttonUp .btn').click((e) => {
+		e.preventDefault()
+
+		window.scrollTo({ top: 0, behavior: 'smooth' })
+	})
+
+
+	// Phone input mask
+	const phoneInputs = document.querySelectorAll('input[type=tel]')
+
+	if (phoneInputs) {
+		phoneInputs.forEach(el => {
+			IMask(el, {
+				mask: '+{7} (000) 000-00-00',
+				lazy: true
+			})
+		})
+	}
+
+
+	// Custom select - Nice select
+	const selects = document.querySelectorAll('select:not(.skip)'),
+		selectsInstances = []
+
+	if (selects) {
+		selects.forEach(el => {
+			selectsInstances.push(NiceSelect.bind(el, {
+				placeholder: el.getAttribute('data-placeholder')
+			}))
+
+			el.addEventListener('change', () => el.classList.add('selected'))
+
+			if (el.querySelector('option[selected]')) {
+				el.classList.add('selected')
+			}
+		})
+	}
+
+
+	if (is_touch_device()) {
+		const subMenus = document.querySelectorAll('header .menu .sub_menu')
+
+		// Submenu on the touch screen
+		$('header .menu_item > a.sub_link').addClass('touch_link')
+
+		$('header .menu_item > a.sub_link').click(function (e) {
+			const dropdown = $(this).next()
+
+			if (dropdown.css('visibility') === 'hidden') {
+				e.preventDefault()
+
+				subMenus.forEach(el => el.classList.remove('show'))
+				dropdown.addClass('show')
+
+				BODY.style = 'cursor: pointer;'
+			}
+		})
+
+		// Close the submenu when clicking outside it
+		document.addEventListener('click', e => {
+			if ($(e.target).closest('.menu').length === 0) {
+				subMenus.forEach(el => el.classList.remove('show'))
+
+				BODY.style = 'cursor: default;'
+			}
+		})
+	}
+
+
+	// Accordion
+	$('body').on('click', '.accordion .accordion_item .head', function(e) {
+		e.preventDefault()
+
+		let item = $(this).closest('.accordion_item'),
+			accordion = $(this).closest('.accordion')
+
+		if (item.hasClass('active')) {
+			item.removeClass('active').find('.data').slideUp(300)
+		} else {
+			accordion.find('.accordion_item').removeClass('active')
+			accordion.find('.data').slideUp(300)
+
+			item.addClass('active').find('.data').slideDown(300)
+		}
+	})
+
+
+	// Product data
+	$('.product_data .item .head').click(function(e) {
+		e.preventDefault()
+
+		const item = $(this).closest('.item')
+
+		item.toggleClass('active')
+		item.find('.data').slideToggle(300)
+	})
+
+
+	// Tabs
+	var locationHash = window.location.hash
+
+	$('body').on('click', '.tabs .btn', function(e) {
+		e.preventDefault()
+
+		if (!$(this).hasClass('active')) {
+			let parent = $(this).closest('.tabs_container'),
+				activeTab = $(this).data('content'),
+				activeTabContent = $(activeTab),
+				level = $(this).data('level')
+
+			parent.find('.tabs:first .btn').removeClass('active')
+			parent.find('.tab_content.' + level).removeClass('active')
+
+			$(this).addClass('active')
+			activeTabContent.addClass('active')
+		}
+	})
+
+	if (locationHash && $('.tabs_container').length) {
+		let activeTab = $(`.tabs button[data-content="${locationHash}"]`),
+			activeTabContent = $(locationHash),
+			parent = activeTab.closest('.tabs_container'),
+			level = activeTab.data('level')
+
+		parent.find('.tabs:first .btn').removeClass('active')
+		parent.find('.tab_content.' + level).removeClass('active')
+
+		activeTab.addClass('active')
+		activeTabContent.addClass('active')
+
+		$('html, body').stop().animate({ scrollTop: $activeTabContent.offset().top }, 1000)
+	}
+
+
+	// Smooth scrolling to anchor
+	const scrollBtns = document.querySelectorAll('.scroll_btn')
+
+	if (scrollBtns) {
+		scrollBtns.forEach(element => {
+			element.addEventListener('click', e => {
+				e.preventDefault()
+
+				let anchor = element.getAttribute('data-anchor')
+
+				if ($(this).closest('.product_info')) {
+					$('#' + anchor).addClass('active').find('.data').slideDown(300)
+				}
+
+				document.getElementById(anchor).scrollIntoView({
+					behavior: 'smooth',
+					block: 'start'
+				}, 1000)
+			})
+		})
+	}
+})
+
+
+
+window.addEventListener('resize', function () {
+	WH = window.innerHeight || document.clientHeight || BODY.clientHeight
+
+	let windowW = window.outerWidth
+
+	if (typeof WW !== 'undefined' && WW != windowW) {
+		// Overwrite window width
+		WW = window.innerWidth || document.clientWidth || BODY.clientWidth
+
+
+		// Mob. version
+		if (!fakeResize) {
+			fakeResize = true
+			fakeResize2 = false
+
+			document.getElementsByTagName('meta')['viewport'].content = 'width=device-width, initial-scale=1, maximum-scale=1'
+		}
+
+		if (!fakeResize2) {
+			fakeResize2 = true
+
+			if (windowW < 375) document.getElementsByTagName('meta')['viewport'].content = 'width=375, user-scalable=no'
+		} else {
+			fakeResize = false
+			fakeResize2 = true
+		}
+	}
+})
